a900bc2d0c202a87d8632dd1408212cc8bfd8470
Alex <chess2ryme@gmail.com>
2017-03-16 11:37:01 +1100

    Various Updates

--- vote/scripts/vote.js
+++ vote/scripts/vote.js
@@ -4,30 +4,82 @@ var app = new Vue({
         candidates: []
     },
     methods: {
-        selectCan: function(can) {
+		contains: function (needle) {
+		    // Per spec, the way to identify NaN is that it is not equal to itself
+		    var findNaN = needle !== needle;
+		    var indexOf;
+
+		    if(!findNaN && typeof Array.prototype.indexOf === 'function') {
+		        indexOf = Array.prototype.indexOf;
+		    } else {
+		        indexOf = function (needle) {
+		            var i = -1, index = -1;
+
+		            for(i = 0; i < this.length; i++) {
+		                var item = this[i];
+
+		                if((findNaN && item !== item) || item === needle) {
+		                    index = i;
+		                    break;
+		                }
+		            }
+
+		            return index;
+		        };
+		    }
+
+		    return indexOf.call(this, needle) > -1;
+		},
+		refresh: function () {
+			setTimeout(function () {
+				app.$http.get('https://sudoapistaging.herokuapp.com/api/pitch/candidates').then(function (response) {
+					var cand = response.body
+					var addTo = [];
+					cand.forEach(function (can) {
+						can.selected = false;
+						can.disabled = false;
+					});
+					cand.forEach(function (can) {
+						if(app.contains(app.candidates, cand)) {
+							console.log("YESSSSS")
+						} else {
+							addTo.push(cand)
+						}
+					})
+
+
+					app.candidates = addTo;
+				}, function (response) {
+					// error callback
+				});
+
+				app.refresh();
+			}, 10000)
+		},
+        selectCan: function (can) {
 
             // Change Selected Value on Object
             if (can.selected) {
-				this.$http.get('https://sudoapistaging.herokuapp.com/api/pitch/downvote/' + can.id).then(function(response) {
-					console.log("Sucess");
-				}, function(response) {
+				this.$http.get('https://sudoapistaging.herokuapp.com/api/pitch/downvote/' + can.id).then(function (response) {
+					console.log("Success");
+				}, function (response) {
 					// error callback
 				});
                 can.selected = false;
             } else {
                 if (!can.disabled) {
-					this.$http.get('https://sudoapistaging.herokuapp.com/api/pitch/vote/' + can.id).then(function(response) {
-						console.log("Sucess");
-					}, function(response) {
+					this.$http.get('https://sudoapistaging.herokuapp.com/api/pitch/vote/' + can.id).then(function (response) {
+						console.log("Success");
+					}, function (response) {
 						// error callback
 					});
                     can.selected = true;
                 }
             }
 
-            var count = function() {
+            var count = function () {
                 var numberOfSelected = 0;
-                app.candidates.forEach(function(can) {
+                app.candidates.forEach(function (can) {
                     if (can.selected == true) {
                         numberOfSelected++;
                     }
@@ -36,13 +88,13 @@ var app = new Vue({
             }();
 
             if (count >= 3) {
-                app.candidates.forEach(function(can) {
+                app.candidates.forEach(function (can) {
                     if (can.selected == false) {
                         can.disabled = true;
                     }
                 });
             } else {
-                app.candidates.forEach(function(can) {
+                app.candidates.forEach(function (can) {
                     can.disabled = false;
                 });
             }
@@ -54,16 +106,18 @@ var app = new Vue({
         }
     },
 	mounted: function () {
-		this.$http.get('https://sudoapistaging.herokuapp.com/api/pitch/candidates').then(function(response) {
+		this.$http.get('https://sudoapistaging.herokuapp.com/api/pitch/candidates').then(function (response) {
 			var cand = response.body
-			cand.forEach(function(can) {
+			cand.forEach(function (can) {
 				can.selected = false;
 				can.disabled = false;
 			});
 			app.candidates = cand;
-		}, function(response) {
+		}, function (response) {
 			// error callback
 		});
 
 	}
 })
+
+app.refresh();
